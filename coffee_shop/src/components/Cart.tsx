86a03acf0c9e164ccf84c577875d999dd55cde22
DVNO <dvno@live.se>
2023-09-12 12:52:06 +0200

    local storage done

--- coffee_shop/src/components/Cart.tsx
+++ coffee_shop/src/components/Cart.tsx
@@ -10,14 +10,37 @@ const Cart = () => {
   const { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart } = useCart()
 
   const onSubmitOrder = () => {
+    // Save the order in local storage
+    const order = {
+      items: cart,
+      totalItems: totalItems,
+      totalPrice: totalPrice,
+      orderDate: new Date().toLocaleString(),
+    }
+
+    // Retrieve existing orders from local storage or create an empty array
+    const existingOrdersJSON = localStorage.getItem('orders')
+    const existingOrders = existingOrdersJSON
+      ? JSON.parse(existingOrdersJSON)
+      : []
+
+    // Add the new order to the existing orders
+    existingOrders.push(order)
+
+    // Save the updated orders array back to local storage
+    localStorage.setItem('orders', JSON.stringify(existingOrders))
+
+    // Dispatch the action to clear the cart
     dispatch({ type: REDUCER_ACTIONS.SUBMIT })
+
+    // Set the confirmation flag
     setConfirm(true)
   }
 
   const pageContent = confirm ? (
     <h2>Thank you for your order.</h2>
   ) : (
-    <div className='cart_container'>
+    <div className="cart_container">
       <h2 className="offscreen">Cart</h2>
       <ul className="cart">
         {cart.map((item) => {
@@ -45,7 +68,7 @@ const Cart = () => {
   )
 
   const content = <main className="main main_cart">{pageContent}</main>
-  
+
   return content
 }
 
