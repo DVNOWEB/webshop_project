69603dc37b75d5fdfcd4675584924f702ac44c22
DVNO <dvno@live.se>
2023-09-13 22:59:11 +0200

    styling done

--- coffee_shop/src/components/ProductDetails.tsx
+++ coffee_shop/src/components/ProductDetails.tsx
@@ -11,18 +11,19 @@ const ProductDetails = ({ product, onClose }: ProductDetailsProps) => {
   const imgSrc: string = new URL(`../images/${product.sku}`, import.meta.url).href
 
   return (
-    <div className="modal-overlay">
+    <div className="modal_overlay">
       <div className="modal">
-        <div className="modal-content">
+        <div className="modal_content">
           <img
             src={imgSrc}
             alt={product.name}
-            className="product-image"
+            className="product_image"
+            onClick={onClose}
           />
           <h2>{product.name}</h2>
           <p>{product.description}</p>
           {/* Add more details here */}
-          <button className="close-modal-btn" onClick={onClose}>
+          <button className="close_modal-btn" onClick={onClose}>
             Close
           </button>
         </div>
