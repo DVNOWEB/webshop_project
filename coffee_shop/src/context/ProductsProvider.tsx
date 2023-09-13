3dbe886111b1248e325a466217b5797d712d087d
DVNO <dvno@live.se>
2023-09-13 12:48:20 +0200

    importing data from json done

--- coffee_shop/src/context/ProductsProvider.tsx
+++ coffee_shop/src/context/ProductsProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactElement, useState } from 'react'
+import { createContext, ReactElement, useState, useEffect } from 'react'
 
 export type ProductType = {
   sku: string
@@ -7,83 +7,6 @@ export type ProductType = {
   price: number
   description: string
 }
-
-// const initState: ProductType[] = []
-
-const initState: ProductType[] = [
-  {
-    sku: 'p1.jpeg',
-    id: 1,
-    name: 'Espresso',
-    price: 1.2,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mauris volutpat, convallis nibh.',
-  },
-  {
-    sku: 'p2.jpg',
-    id: 2,
-    name: 'Doppio',
-    price: 2,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mauris volutpat, convallis nibh.',
-  },
-  {
-    sku: 'p3.jpg',
-    id: 3,
-    name: 'Macchiato',
-    price: 2.5,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mauris volutpat, convallis nibh.',
-  },
-  {
-    sku: 'p4.jpg',
-    id: 4,
-    name: 'Americano',
-    price: 2,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mauris volutpat, convallis nibh.',
-  },
-  {
-    sku: 'p5.jpg',
-    id: 5,
-    name: 'Cappuccino',
-    price: 3,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mauris volutpat, convallis nibh.',
-  },
-  {
-    sku: 'p6.jpg',
-    id: 6,
-    name: 'Latte',
-    price: 3.5,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mauris volutpat, convallis nibh.',
-  },
-  {
-    sku: 'p7.jpg',
-    id: 7,
-    name: 'Affogato',
-    price: 4.5,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mauris volutpat, convallis nibh.',
-  },
-  {
-    sku: 'p8.jpeg',
-    id: 8,
-    name: 'Mocha',
-    price: 3.3,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mauris volutpat, convallis nibh.',
-  },
-  {
-    sku: 'p9.jpg',
-    id: 9,
-    name: 'Flat White',
-    price: 3,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mauris volutpat, convallis nibh.',
-  },
-]
 
 export type UseProductsContextType = { products: ProductType[] }
 
@@ -94,27 +17,30 @@ const ProductsContext = createContext<UseProductsContextType>(initContextState)
 type ChildrenType = { children?: ReactElement | ReactElement[] }
 
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
-  const [products] = useState<ProductType[]>(initState)
-  
-  // const [products, setProducts] = useState<ProductType[]>(initState)
+  const [products, setProducts] = useState<ProductType[]>(
+    initContextState.products
+  )
 
-  // useEffect(() => {
-  //   const fetchProducts = async (): Promise<ProductType[]> => {
-  //     const data = await fetch('http://localhost:3000/products')
-  //       .then((res) => {
-  //         return res.json()
-  //       })
-  //       .catch((err) => {
-  //         if (err instanceof Error) console.log(err.message)
-  //       })
-  //     return data
-  //   }
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      try {
+        // Fetch the data from the imported JSON file
+        const data = await fetch('/data/products.json')
+        if (!data.ok) {
+          throw new Error('Failed to fetch products')
+        }
+        const productsData = await data.json()
+        return productsData.products // Assuming the products are stored in a 'products' property
+      } catch (error) {
+        console.error('Error fetching products:', error)
+        return []
+      }
+    }
 
-
-  //   fetchProducts().then(products => setProducts(products))
-  // }, [])
-  
-
+    fetchProducts().then((fetchedProducts) => {
+      setProducts(fetchedProducts)
+    })
+  }, [])
 
   return (
     <ProductsContext.Provider value={{ products }}>
